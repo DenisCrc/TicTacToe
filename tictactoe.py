934108a630d44ee4289cdf4ea28aa4b7a057b05b
Denis-Gabriel Craciun <denny.craciun@gmail.com>
2025-10-19 02:54:48 +0300

    Implement player selection and move input
    Added player input for choosing player type and handling moves.

--- tictactoe.py
+++ tictactoe.py
@@ -1,1 +1,41 @@
+print("Do you want to be player 1(x) or player 2(O)?")
+p=int(input())
+def create_board():
+    """Create and return a 3x3 Tic-Tac-Toe board."""
+    board = [
+        [' ', ' ', ' '],
+        [' ', ' ', ' '],
+        [' ', ' ', ' ']
+    ]
+    return board
+board=create_board()
 
+def print_board(board):
+    for i, row in enumerate(board):
+        print(" | ".join(row))
+        if i < 2:
+            print("---+---+---")
+        print()
+
+def player_move(board, player):
+    print("Please enter your move (row and column, 0-2):")
+    row = int(input())
+    column = int(input())
+
+    if row>2 | column>2:
+        print("Please enter a valid move")
+        row = int(input())
+        column = int(input())
+    else:
+        if board[row][column] != ' ':
+            print("Please enter a valid move")
+            row = int(input())
+            column = int(input())
+    
+    if player == 1:
+        board[row][column] = 'X'
+    else:
+        board[row][column] = 'O'
+
+player_move(board,p)
+print_board(board)
